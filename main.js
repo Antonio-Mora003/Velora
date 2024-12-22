44110aaed2c31e0219502133f37f07043259211c
Antonio-Mora003 <antoniomor42003@gmail.com>
2024-12-22 05:55:14 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+window.sr = ScrollReveal()
+
+sr.reveal('.about_name', {
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 300,
+    easing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+})
+
+sr.reveal('.about_title',{
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 400,
+    easing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+
+})
+
+
+
+sr.reveal('.about_subtitle',{
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 500,
+    easing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+
+})
+sr.reveal('.about_paragraph',{
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 600,
+    easing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+
+})
+
+sr.reveal('.button',{
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 700,
+    easing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+
+})
+sr.reveal('.img-1',{
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 1300,
+    easing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+
+})
+sr.reveal('.img-2',{
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 1500,
+    easing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+
+})
